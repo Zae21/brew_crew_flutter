8fec6c42d589f52d0c9d2a3f31e8885d53a315c0
Zaenal A. Prayoga <prayoga96@gmail.com>
2021-02-17 15:53:43 +0700

    add new section

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:brew_crew/screens/wrapper.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Brew Crew',
-    home: Text('Text'),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Brew Crew', home: Wrapper());
+  }
 }
