c94575cd73d78d7e9c390539ded5cf3f0537d75d
Zaenal A. Prayoga <prayoga96@gmail.com>
2021-02-18 17:07:26 +0700

    add new section

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:brew_crew/screens/wrapper.dart';
+import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,26 +13,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Brew Crew',
-        home: FutureBuilder(
-          future: Firebase.initializeApp(),
-          builder: (context, snapshot) {
-            // Check for errors
-            if (snapshot.hasError) {
-              print(snapshot.hasError);
-              return Container();
-            }
-
-            // Once complete, show your application
-            if (snapshot.connectionState == ConnectionState.done) {
-              return Wrapper();
-            }
-
-            // Otherwise, show something whilst waiting for initialization to complete
-            // return Loading();
-            return Container();
-          },
-        ));
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          print(snapshot.hasError);
+          return Container();
+        }
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return StreamProvider<Ouser>.value(
+              value: AuthService().user,
+              child: MaterialApp(
+                title: 'Brew Crew',
+                home: Wrapper(),
+              ));
+        }
+        // Otherwise, show something whilst waiting for initialization to complete
+        // return Loading();
+        return Container();
+      },
+    );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,9 +1,13 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:brew_crew/screens/auth/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<Ouser>(context);
+    print(user);
     return Auth();
   }
 }
