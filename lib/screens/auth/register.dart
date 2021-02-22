fc31dfc483f723cb1d51530c09e4b91955c2bb89
zae21 <prayoga96@gmail.com>
2021-02-23 00:08:15 +0700

    add new sections

--- lib/screens/auth/register.dart
+++ lib/screens/auth/register.dart
@@ -1,4 +1,6 @@
 import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/shared/constans.dart';
+import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -13,80 +15,93 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool isLoading = false;
 
   String email = '';
   String password = '';
   String errorMsg = '';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text('Sign Up Into Brew Cerew'),
-        actions: <Widget>[
-          FlatButton.icon(
-            onPressed: () {
-              widget.toggleView();
-            },
-            icon: Icon(Icons.person),
-            label: Text('Sign In'),
-          )
-        ],
-      ),
-      body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 20.0),
-                TextFormField(
-                  validator: (val) {
-                    if (val.isEmpty) return 'Email tidak boleh dikosongkan.';
-                    return null;
+    return isLoading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.grey[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text('Sign Up Into Brew Cerew'),
+              actions: <Widget>[
+                FlatButton.icon(
+                  onPressed: () {
+                    widget.toggleView();
                   },
-                  onChanged: (val) {
-                    setState(() => email = val);
-                  },
-                ),
-                SizedBox(height: 20.0),
-                TextFormField(
-                  validator: (val) {
-                    if (val.isEmpty) return 'Pasword tidak boleh dikosongkan.';
-                    if (val.length < 6)
-                      return 'Pasword tidak boleh kurang dari 6 karakter.';
-                    return null;
-                  },
-                  obscureText: true,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  },
-                ),
-                SizedBox(height: 20.0),
-                RaisedButton(
-                  onPressed: () async {
-                    if (_formKey.currentState.validate()) {
-                      dynamic result =
-                          await _auth.signUpEmailPassword(email, password);
-                      if (result == null) {
-                        setState(() => errorMsg = 'Invalid email format');
-                      }
-                    }
-                  },
-                  color: Colors.pink[400],
-                  child: Text('Register',
-                      style: TextStyle(
-                        color: Colors.white,
-                      )),
-                ),
-                SizedBox(height: 15.0),
-                Text(errorMsg,
-                    style: TextStyle(color: Colors.red, fontSize: 14.0))
+                  icon: Icon(Icons.person),
+                  label: Text('Sign In'),
+                )
               ],
             ),
-          )),
-    );
+            body: Container(
+                padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Email'),
+                        validator: (val) {
+                          if (val.isEmpty)
+                            return 'Email tidak boleh dikosongkan.';
+                          return null;
+                        },
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Password'),
+                        validator: (val) {
+                          if (val.isEmpty)
+                            return 'Pasword tidak boleh dikosongkan.';
+                          if (val.length < 6)
+                            return 'Pasword tidak boleh kurang dari 6 karakter.';
+                          return null;
+                        },
+                        obscureText: true,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      RaisedButton(
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            setState(() => isLoading = true);
+                            dynamic result = await _auth.signUpEmailPassword(
+                                email, password);
+                            if (result == null) {
+                              setState(() {
+                                errorMsg = 'Invalid Email format.';
+                                isLoading = true;
+                              });
+                            }
+                          }
+                        },
+                        color: Colors.pink[400],
+                        child: Text('Register',
+                            style: TextStyle(
+                              color: Colors.white,
+                            )),
+                      ),
+                      SizedBox(height: 15.0),
+                      Text(errorMsg,
+                          style: TextStyle(color: Colors.red, fontSize: 14.0))
+                    ],
+                  ),
+                )),
+          );
   }
 }
--- lib/screens/auth/sign-in.dart
+++ lib/screens/auth/sign-in.dart
@@ -1,4 +1,6 @@
 import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/shared/constans.dart';
+import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -13,81 +15,93 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool isLoading = false;
 
   String email = '';
   String password = '';
   String errorMsg = '';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text('Sign In Into Brew Cerew'),
-        actions: <Widget>[
-          FlatButton.icon(
-            onPressed: () {
-              widget.toggleView();
-            },
-            icon: Icon(Icons.person),
-            label: Text('Sign Up'),
-          )
-        ],
-      ),
-      body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 20.0),
-                TextFormField(
-                  validator: (val) {
-                    if (val.isEmpty) return 'Email tidak boleh dikosongkan.';
-                    return null;
+    return isLoading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.grey[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text('Sign In Into Brew Cerew'),
+              actions: <Widget>[
+                FlatButton.icon(
+                  onPressed: () {
+                    widget.toggleView();
                   },
-                  onChanged: (val) {
-                    setState(() => email = val);
-                  },
-                ),
-                SizedBox(height: 20.0),
-                TextFormField(
-                  validator: (val) {
-                    if (val.isEmpty) return 'Pasword tidak boleh dikosongkan.';
-                    if (val.length < 6)
-                      return 'Pasword tidak boleh kurang dari 6 karakter.';
-                    return null;
-                  },
-                  obscureText: true,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  },
-                ),
-                SizedBox(height: 20.0),
-                RaisedButton(
-                  onPressed: () async {
-                    if (_formKey.currentState.validate()) {
-                      dynamic result =
-                          await _auth.signInEmailPassword(email, password);
-                      if (result == null) {
-                        setState(
-                            () => errorMsg = 'Email or Password not found.');
-                      }
-                    }
-                  },
-                  color: Colors.pink[400],
-                  child: Text('Sign In',
-                      style: TextStyle(
-                        color: Colors.white,
-                      )),
-                ),
-                SizedBox(height: 15.0),
-                Text(errorMsg,
-                    style: TextStyle(color: Colors.red, fontSize: 14.0))
+                  icon: Icon(Icons.person),
+                  label: Text('Sign Up'),
+                )
               ],
             ),
-          )),
-    );
+            body: Container(
+                padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Email'),
+                        validator: (val) {
+                          if (val.isEmpty)
+                            return 'Email tidak boleh dikosongkan.';
+                          return null;
+                        },
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Password'),
+                        validator: (val) {
+                          if (val.isEmpty)
+                            return 'Pasword tidak boleh dikosongkan.';
+                          if (val.length < 6)
+                            return 'Pasword tidak boleh kurang dari 6 karakter.';
+                          return null;
+                        },
+                        obscureText: true,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      RaisedButton(
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            setState(() => isLoading = true);
+                            dynamic result = await _auth.signInEmailPassword(
+                                email, password);
+                            if (result == null) {
+                              setState(() {
+                                errorMsg = 'Credential not found.';
+                                isLoading = true;
+                              });
+                            }
+                          }
+                        },
+                        color: Colors.pink[400],
+                        child: Text('Sign In',
+                            style: TextStyle(
+                              color: Colors.white,
+                            )),
+                      ),
+                      SizedBox(height: 15.0),
+                      Text(errorMsg,
+                          style: TextStyle(color: Colors.red, fontSize: 14.0))
+                    ],
+                  ),
+                )),
+          );
   }
 }
