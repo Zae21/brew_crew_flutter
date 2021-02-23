8c92d89e08398e7d43ebcdc44fabb44e41c98679
Zaenal A. Prayoga <prayoga96@gmail.com>
2021-02-23 11:29:27 +0700

    add new sections

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:brew_crew/models/brew.dart';
 
 class DatabaseService {
   final String uid;
@@ -12,5 +13,19 @@ class DatabaseService {
     return await brewsCollection
         .doc(uid)
         .set({'sugar': sugar, 'name': name, 'strength': strength});
+  }
+
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.docs.map((doc) {
+      return Brew(
+        name: doc['name'] ?? '',
+        strength: doc['strength'] ?? 0,
+        sugar: doc['sugar'] ?? '0',
+      );
+    });
+  }
+
+  Stream<List<Brew>> get brews {
+    return brewsCollection.snapshots().map(_brewListFromSnapshot);
   }
 }
